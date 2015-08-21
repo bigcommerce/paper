2da6b3096bcc838d67f4039daa6579a581b8435e
Mario Campa <mario.campa@bigcommerce.com>
2015-08-21 13:50:22 -0700

    Deprecating unnecessary helpers

--- helpers/deprecated.js
+++ helpers/deprecated.js
@@ -1,4 +1,22 @@
+var _ = require('lodash');
+
 module.exports = function (paper) {
+
+    paper.handlebars.registerHelper('pick', function(object, predicate) {
+        return _.pick.apply(null, arguments);
+    });
+
+    paper.handlebars.registerHelper('enumerate', function(start, end, options) {
+        var out = '',
+            i = start,
+            iOut;
+
+        for (i; i <= end; i++) {
+            out = out + options.fn(i);
+        }
+
+        return out + '';
+    });
 
     paper.handlebars.registerHelper('getShortMonth', function (index) {
 
