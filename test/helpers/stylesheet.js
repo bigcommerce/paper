cfab49eba4f29fca787ab87a60cf47984aa75c2a
Mario Campa <mario.campa@bigcommerce.com>
2015-11-05 15:33:48 -0800

    Theme editor temporary fix

--- test/helpers/stylesheet.js
+++ test/helpers/stylesheet.js
@@ -21,21 +21,21 @@ describe('stylesheet helper', function() {
 
     it('should render a link tag with the cdn ulr and stencil-stylesheet data tag', function(done) {
         expect(c('{{{stylesheet "assets/css/style.css"}}}', context))
-            .to.be.equal('<link data-stencil-stylesheet href="https://cdn.bcapp/hash/stencil/1/2/css/style.css" rel="stylesheet">');
+            .to.be.equal('<link data-stencil-stylesheet href="https://cdn.bcapp/hash/stencil/1/2/css/style.css" rel="stylesheet" id="https://cdn.bcapp/hash/stencil/1/2/css/style.css">');
 
         done();
     });
 
     it('should render a link tag and all extra attributes with no cdn url', function(done) {
         expect(c('{{{stylesheet "assets/css/style.css" rel="something" class="myStyle"}}}', {}))
-            .to.be.equal('<link data-stencil-stylesheet href="/assets/css/style.css" rel="something" class="myStyle">');
+            .to.be.equal('<link data-stencil-stylesheet href="/assets/css/style.css" rel="something" class="myStyle" id="/assets/css/style.css">');
 
         done();
     });
 
     it('should render a link with empty href', function(done) {
         expect(c('{{{stylesheet "" }}}', context))
-            .to.be.equal('<link data-stencil-stylesheet href="" rel="stylesheet">');
+            .to.be.equal('<link data-stencil-stylesheet href="" rel="stylesheet" id="">');
 
         done();
     });
