10c43081b5321434731dada9a995b7803288f4a0
Mario Campa <mcampa@gmail.com>
2016-10-19 11:02:09 -0700

    MERC-1372 fix uncaught error happening if someone does not pass a name to the block/partial helper

--- helpers/block.js
+++ helpers/block.js
@@ -1,13 +1,18 @@
 'use strict';
+var _ = require('lodash');
 
 function helper(paper) {
-    paper.handlebars.registerHelper('block', function (name, options) {
+    paper.handlebars.registerHelper('block', function (name) {
+        const options = arguments[arguments.length - 1];
+
         /* Look for partial by name. */
         var partial = paper.handlebars.partials[name] || options.fn;
         return partial(this, {data: options.hash});
     });
 
-    paper.handlebars.registerHelper('partial', function (name, options) {
+    paper.handlebars.registerHelper('partial', function (name) {
+        var options = arguments[arguments.length - 1];
+
         paper.handlebars.registerPartial(name, options.fn);
     });
 }
