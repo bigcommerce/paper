afa2f50123849cf59c3e1ebda318f8aad944427d
bc-zoharmuzafi <zohar.muzafi@bigcommerce.com>
2019-04-16 14:17:04 -0700

    change concat to return a string

--- helpers/concat.js
+++ helpers/concat.js
@@ -6,7 +6,12 @@
  *  {{@lang (concat 'products.reviews.rating.' this) }}
  */
 function helper(paper) {
-    paper.handlebars.registerHelper('concat', function (value, otherValue) {
+    paper.handlebars.registerHelper('concat', function (value, otherValue, returnSafeString =  true) {
+        // if returnSafeString set to true the returned value will be from SafeString type
+        if (returnSafeString) {
+            return new paper.handlebars.SafeString(value + otherValue);
+        }
+
         return `${value}${otherValue}`;
     });
 }
