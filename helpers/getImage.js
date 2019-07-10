014752c85feb088e33f0df1212a8c7dcf08b9d73
Nathan Booker <nathan.booker@bigcommerce.com>
2019-07-10 17:43:25 -0500

    Pull in other image updates

--- helpers/getImage.js
+++ helpers/getImage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var _ = require('lodash');
+const SafeString = require('handlebars').SafeString;
+const common = require('./../lib/common');
 
 function helper(paper) {
     paper.handlebars.registerHelper('getImage', function (image, presetName, defaultImageUrl) {
@@ -11,7 +13,8 @@ function helper(paper) {
         var width;
         var height;
 
-        if (!_.isPlainObject(image) || !_.isString(image.data) || image.data.indexOf('{:size}') === -1) {
+        if (!_.isPlainObject(image) || !_.isString(image.data)
+            || !common.isValidURL(image.data) || image.data.indexOf('{:size}') === -1) {
             // return empty string if not a valid image object
             defaultImageUrl = defaultImageUrl ? defaultImageUrl : '';
             return _.isString(image) ? image : defaultImageUrl;
@@ -19,8 +22,8 @@ function helper(paper) {
 
         if (_.isPlainObject(presets) && _.isPlainObject(presets[presetName])) {
             // If preset is one of the given presets in _images
-            width = parseInt(presets[presetName].width, 10) || 4096;
-            height = parseInt(presets[presetName].height, 10) || 4096;
+            width = parseInt(presets[presetName].width, 10) || 5120;
+            height = parseInt(presets[presetName].height, 10) || 5120;
             size = width + 'x' + height;
 
         } else if (sizeRegex.test(settings[presetName])) {
@@ -31,7 +34,7 @@ function helper(paper) {
             size = 'original';
         }
 
-        return image.data.replace('{:size}', size);
+        return new SafeString(image.data.replace('{:size}', size));
     });
 };
 
